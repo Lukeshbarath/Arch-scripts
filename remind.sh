859325c7fc071f5f283dff18a883a05e36858798
Luke <stlukeshbarath@gmail.com>
2026-05-12 23:34:57 +0530

    Remind.sh - feat:Added interactive user input

--- remind.sh
+++ remind.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-message="$1"
-time="$2"
 
-sleep "$time"
+echo "Enter message: "
+read msg
 
-notify-send "Reminder" "$message"
+echo "Enter duration in hours(h)/minutes(m)/seconds(s) {as 1 m/1 h/40 s}: "
+read period unit
+if [ $unit ==  h ]; then 
+	period_sec=$((period*60*60))
+elif [ $unit ==   m ]; then
+	period_sec=$((period*60))
+elif [ $unit ==  s ]; then
+	period_sec=$period
+fi
 
+echo $period_sec
+
+{
+sleep "$period_sec" 
+notify-send "Reminder" "$msg" 
+} &
